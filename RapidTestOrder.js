6c4e88c7c14c93ffe46f5f12bf125c187b09e8e4
abiegbeyemi <abibat.egbeyemi@gmail.com>
2024-02-08 21:01:50 -0500

    changed rapid test to drinks

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -4,8 +4,8 @@ class RapidTestOrder {
       WELCOMING: () => {
         let aReturn = [];
         this.stateCur = this.OrderState.RESERVING;
-        aReturn.push("Welcome to Rich's Rapid Test.");
-        aReturn.push("Would you like to reserve a rapid test kit?");
+        aReturn.push("Welcome to Fridays Cafe and Eatery.");
+        aReturn.push("What type of drink would you like? We have a bit of everything here!");
         return aReturn;
       },
       RESERVING: (sInput) => {
