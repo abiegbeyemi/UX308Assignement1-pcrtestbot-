a408fc193aaa3166385246bc89b52d11e4dd9017
abiegbeyemi <abibat.egbeyemi@gmail.com>
2024-02-16 10:04:08 -0500

    bagel and drink type

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -1,52 +1,55 @@
 class RapidTestOrder {
-  constructor(sFrom) {
-    this.OrderState = {
-      WELCOMING: () => {
-        let aReturn = [];
-        this.stateCur = this.OrderState.FOOD_TYPE;
-        aReturn.push("Welcome to Fridays Cafe and Eatery.");
-        aReturn.push("What can I get for you today? We have Breakfast bagels or Panini wraps!");
-        return aReturn;
-      },
+    constructor(sFrom) {
+        this.OrderState = {
+            WELCOMING: () => {
+                let aReturn = [];
+                this.stateCur = this.OrderState.FOOD_TYPE;
+                aReturn.push("Welcome to Fridays Cafe and Eatery.");
+                aReturn.push("What can I get for you today? We have Breakfast bagels or Panini wraps!");
+                return aReturn;
+            },
 
-      FOOD_TYPE: (sInput) =>{
-        let aReturn = [];
-        this.isDone = [];
-        if (sInput.toLowerCase().startsWith('b')) {
-            aReturn.push("Nice choice! What kind of bagel would you like?");
-        } else {
-            aReturn.push("Cold? We have Iced Coffee or Mango Chai Smoothie.");
-            aReturn.push("Maybe next time")
+            FOOD_TYPE: (sInput) => {
+                let aReturn = [];
+                this.isDone = [];
+                if (sInput.toLowerCase().startsWith('b')) {
+                    aReturn.push("Nice choice! What kind of bagel would you like?");
+                    aReturn.push("We have plain, seasme, jalapeno, and everything.")
+                    this.stateCur = this.OrderState.BAGEL_TYPE;
+                } else {
+                    aReturn.push("Nice choice! What kind of Panini would you like?");
+
+                }
+                return aReturn;
+            },
+
+            BAGEL_TYPE: (sInput) => {
+                let aReturn = [];
+                aReturn.push("Would you like a drink with that?")
+                aReturn.push("We have small, medium, and large!")
+                this.stateCur = this.OrderState.DRINK_TYPE;
+                return aReturn;
+
+            },
+
+            DRINK_TYPE: (sInput) => {
+                let aReturn = [];
+                aReturn.push("Thank you for your order! It will be ready in 20 minutes!")
+                return aReturn;
+            }
         }
-        return aReturn;
-        }, 
 
-      RESERVING: (sInput) => {
-        let aReturn = [];
-        this.isDone = true;
-        if (sInput.toLowerCase().startsWith('y')) {
-          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
-          let d = new Date();
-          d.setMinutes(d.getMinutes() + 120);
-          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
-        } else {
-          aReturn.push("Thanks for trying our reservation system");
-          aReturn.push("Maybe next time")
-        }
-        return aReturn;
-      }
-    };
 
-    this.stateCur = this.OrderState.WELCOMING;
-    this.isDone = false;
-    this.sFrom = sFrom;
-  }
-  handleInput(sInput) {
-    return this.stateCur(sInput);
-  }
-  isDone() {
-    return this.isDone;
-  }
+        this.stateCur = this.OrderState.WELCOMING;
+        this.isDone = false;
+        this.sFrom = sFrom;
+    }
+    handleInput(sInput) {
+        return this.stateCur(sInput);
+    }
+    isDone() {
+        return this.isDone;
+    }
 }
 
 export { RapidTestOrder }
